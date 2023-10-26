a37f80303ce402754ff74fe715ca109fdb35d730
Victor Senishyn <senishin2004@gmail.com>
2023-10-26 20:29:38 +0300

    lb8

---
+++ script/script.js
@@ -0,0 +1,51 @@
+let userName = prompt('Enter your name');
+let leftScore = 0;
+let rightScore = 0;
+
+if (userName === null || userName === '') {
+    userName = 'Player';
+}
+document.getElementById('player-name').textContent = userName;
+
+function generate() {
+    let leftNum = parseInt(Math.random() * 10);
+    let rightNum = parseInt(Math.random() * 10);
+
+	document.querySelector('.right-c').classList.remove('shake-element')
+	document.querySelector('.left-c').classList.remove('shake-element')
+
+    document.getElementById('left-num').textContent = leftNum;
+    document.getElementById('right-num').textContent = rightNum;
+
+    if (leftNum > rightNum) {
+        leftScore++;
+    } else if (rightNum > leftNum) {
+        rightScore++;
+    }
+
+    document.getElementById('left-score').textContent = leftScore;
+    document.getElementById('right-score').textContent = rightScore;
+
+    if (leftScore == 3) {
+	    document.querySelector('.left-c').classList.add('shake-element')
+        alert(`${userName} won`);
+        toStartPosition();
+    } else if (rightScore == 3) {
+	    document.querySelector('.right-c').classList.add('shake-element')
+		alert('Computer won');
+        toStartPosition();
+    } else if (rightScore == 3 && leftScore == 3) {
+        alert('Draw');
+        toStartPosition();
+    }
+}
+
+function toStartPosition() {
+    leftScore = 0;
+    rightScore = 0;
+    document.getElementById('left-score').textContent = leftScore;
+    document.getElementById('right-score').textContent = rightScore;
+
+    document.getElementById('left-num').textContent = 0;
+    document.getElementById('right-num').textContent = 0;
+}
