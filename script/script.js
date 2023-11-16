ce456d9d2b9b1cc7c81ee35b7dbe234a53e62b39
Victor Senishyn <senishin2004@gmail.com>
2023-11-16 22:46:37 +0200

    Fix: removed unnecessary file and added new file

--- script/script.js
+++ script/script.js
@@ -1,115 +1,82 @@
+let countTrue = 0
+let countFalse = 0
+let num = 0
+
 let userName = prompt('Enter your name')
-let countButtonPress = 0
-
-const generatorButton = document.getElementById('btn')
-generatorButton.addEventListener('click', generate)
-
-if (userName === null || userName === '') {
+if (userName === null || userName.trim() === '') {
 	userName = 'Player'
 }
-document.getElementById('player-name').textContent = userName
+$('#user-name').text(userName)
 
-function generate() {
-	countButtonPress++
+const questionAndAnswer = {
+	question: ['snow','red','stone','tree','sky','ocean','mountain','flower','fire','book','sun','moon','star','cloud','wind'],
+	answer: ['сніг','червоний','каміння','дерево','небо','океан','гора','квітка','вогонь','книга','сонце','місяць','зірка','хмара','вітер'],
+}
 
-	let content11 = document.getElementById('el11').innerHTML
-	let content21 = document.getElementById('el21').innerHTML
-	let content31 = document.getElementById('el31').innerHTML
+let startSize = questionAndAnswer.question.length
+$('#number').text(
+	`${startSize}/0`		
+)
+let rand = parseInt(Math.random() * questionAndAnswer.question.length)
 
-	let content12 = document.getElementById('el12').innerHTML
-	let content22 = document.getElementById('el22').innerHTML
-	let content32 = document.getElementById('el32').innerHTML
+$('#question').text(questionAndAnswer.question[rand])
+$('#btn').on('click', ()=>{
+	const userAnswer = String($('#answer').val()).trim().toLowerCase()//val для отримання значення з input
+	const correctAnswer = questionAndAnswer.answer[rand].toLowerCase()
 
-	let content13 = document.getElementById('el13').innerHTML
-	let content23 = document.getElementById('el23').innerHTML
-	let content33 = document.getElementById('el33').innerHTML
 
-	swapContent(
-		content11,
-		content21,
-		content31,
-		parseInt(Math.random() * 13) + 5,
-		'el11',
-		'el21',
-		'el31'
-	)
-	swapContent(
-		content12,
-		content22,
-		content32,
-		parseInt(Math.random() * 13) + 5,
-		'el12',
-		'el22',
-		'el32'
-	)
-	swapContent(
-		content13,
-		content23,
-		content33,
-		parseInt(Math.random() * 13) + 5,
-		'el13',
-		'el23',
-		'el33'
-	)
-
-	generatorButton.removeEventListener('click', generate)
-	setTimeout(()=>{
-		let el21Value = document.getElementById('el21').textContent.trim()
-		let el22Value = document.getElementById('el22').textContent.trim()
-		let el23Value = document.getElementById('el23').textContent.trim()
-		if (el21Value === el22Value && el22Value === el23Value) {
-			alert(`${userName} WON`)
-			countButtonPress = 0
-			setTimeout(() => {
-				document
-					.querySelector('.shape-for-form')
-					.classList.add('change-color-animation')
-			}, 3000)
-			document
-				.querySelector('.shape-for-form')
-				.classList.remove('change-color-animation')
-
-		} else if (countButtonPress === 3){
-			alert('TRY AGAIN')
-			countButtonPress = 0
+	if (userAnswer === correctAnswer) {
+		countTrue++
+		num++
+		let temp = {
+			question: [],
+			answer: [],
 		}
-		generatorButton.addEventListener('click', generate)
-	}, 4000)
-}
 
-function swapContent(
-	content1,
-	content2,
-	content3,
-	numberOfScrolls,
-	el1,
-	el2,
-	el3
-) {
-	let temp = content3
-	content3 = content2
-	content2 = content1
-	content1 = temp
+		let i = 0
+		let delElement
+		for (let item of questionAndAnswer.answer) {
+			if (correctAnswer !== item) {
+				temp.answer[i++] = item
+			} else {
+				delElement = i
+			}
+		}
 
-	document.getElementById(el1).innerHTML = content1
-	document.getElementById(el2).innerHTML = content2
-	document.getElementById(el3).innerHTML = content3
-
-	numberOfScrolls--
-
-	if (numberOfScrolls > 0) {
-		setTimeout(
-			() =>
-				swapContent(
-					content1,
-					content2,
-					content3,
-					numberOfScrolls,
-					el1,
-					el2,
-					el3
-				),
-			200
-		)
+		i = 0
+		for (let j = 0; j < questionAndAnswer.question.length; j++) {
+			if (j !== delElement) {
+				temp.question[i++] = questionAndAnswer.question[j]
+			}
+		}
+		questionAndAnswer.question = temp.question
+		questionAndAnswer.answer = temp.answer
+	} else {
+		alert('Wrong answer')
+		countFalse++
 	}
-}
+	$('#answer').val('')
+
+	$('#number').text(
+		`${startSize}/${num}`
+	)
+	$('#score').text(
+		`True:${countTrue} False:${countFalse}`
+	)
+
+	if (questionAndAnswer.question.length === 0) {
+		$('#question').text(`Test over`)
+		if (countFalse === 0) {
+			alert('Your level of English is very good')
+		} else if (countFalse < 6) {
+			alert('Your level of English is good')
+		} else if (countFalse < 11) {
+			alert('Your level of English is bad')
+		} else {
+			alert('Your level of English is very bad')
+		}
+	}else{
+		rand = parseInt(Math.random() * questionAndAnswer.question.length)
+		$('#question').text(questionAndAnswer.question[rand])
+	}
+})
